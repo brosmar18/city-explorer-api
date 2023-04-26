2909bca327a8e485d1a37c01610a8ebe79a8ce0c
Bryan Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-04-26 14:39:47 -0500

    /weatther added to proocess, lat, lon, and searchQuery info

--- server.js
+++ server.js
@@ -1,27 +1,62 @@
 'use strict';
 console.log('!!server.js connected.');
 
+// REQUIRE
+// In our server, we have to use 'require' instead of import'
+// Here we will list the requirement for a server.
+
 //1. npm i express
 const express = require('express');
 //how wee bring the env file.
 require('dotenv').config();
-let weatherData = require('./data/weather.json');
-
-//Now include cors to share resources over the web.
 const cors = require('cors');
 
-//2.
+// Two things to get a server running
+// npm i dotenv - define our port, validate that my .env file is working.
+// if server runs on 5005, I know something is wrong with my env file.
+// Set up a PORT to handle functionality.
+const PORT = process.env.PORT || 5005;
+
+
+// Add data
+let data = require('./data/weather.json');
+
+// Use
+//Express takes 2 steps: 'require' and 'use'.
 const app = express();
 app.use(cors());
 
-// npm i dotenv = define our port, validat that my .env file is working.
-//if server runs on 3002, I know something is wrong with my env file.
-const PORT = process.env.PORT || 5005;
 
 app.get('/', (request, response) => {
-    //then we need to send something back.
-    response.send('hello from our server!!');
+  response.send('Hello from our server file!!');
 });
+
+app.get('/weather', (request, response) => {
+  //http://localhost:3002/weather?lat=47.60621&lon=-122.33207&searchQuery=Seattle
+  console.log('req object', request.query.searchQuery);
+  let lat = request.query.lat;
+  let lon = request.query.lon;
+  console.log(lat, lon);
+
+  response.send(`Lattitute: ${lat} Longtitute: ${lon}`);
+});
+
+// Add route to handle the front end request.
+
+
+
+
+// Add class to process the data before we send it back to the front.
+// Class and Constructor.
+
+
+
+
+//handles all route requests when we don't have that route.
+app.get('*', (request, response) => {
+  response.send('The route was not found. Error 404');
+});
+
 
 
 // Listen for port to start the serveer.
